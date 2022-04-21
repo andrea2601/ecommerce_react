fd978885e39218d3fd8eb698789e55d3626b07db
andrea2601 <andrealogiudice26@gmail.com>
2022-04-21 18:49:18 +0200

    some fix

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,7 +4,7 @@ const mock = ["category 1", "category 2", "category 3"];
 
 export const Sidebar = (props) => {
   const [categories, setCategories] = useState(mock);
-  const [isActive, setActive] = useState(1); 
+  const [isActive, setActive] = useState(0); 
 
   const getData = async () => {
     const response = await fetch(
@@ -25,9 +25,9 @@ export const Sidebar = (props) => {
     props.catSelection(category);
     
     setActive(index);
-    console.log("isactive: ",isActive);
-    console.log("event: ",event);
-    event.target.className="active";
+    // console.log("isactive: ",isActive);
+    // console.log("event: ",event);
+    // event.target.className="active";
   };
 
   return (
